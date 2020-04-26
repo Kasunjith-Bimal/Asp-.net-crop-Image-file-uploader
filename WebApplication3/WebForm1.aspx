3346633365e24d0a71886f7fb056e49a7bccf94a
Kasunjith-Bimal <kasunysoft@gmail.com>
2020-04-26 23:54:14 +0530

    add comment

---
+++ WebApplication3/WebForm1.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    
+    <title></title>
+    <script src="Scripts/jquery-3.4.1.js"></script>
+    <script src="Scripts/jquery.Jcrop.js"></script>
+    <link href="Content/jquery.Jcrop.css" rel="stylesheet" />
+    <script type="text/javascript">
+        function showpreview(input) {
+
+            if (input.files && input.files[0]) {
+
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('#imgpreview').css('visibility', 'visible');
+                    $('#imgpreview').attr('src', e.target.result);
+                    $('#imgpreview').Jcrop({
+                        setSelect: [0, 0, 200, 100],
+                        onChange: SetCoordinates,
+                        onSelect: SetCoordinates,
+                        allowResize: true
+                    });
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+
+        }
+
+
+        function SetCoordinates(c) {
+            $('#imgX1').val(c.x);
+            $('#imgY1').val(c.y);
+            $('#imgWidth').val(c.w);
+            $('#imgHeight').val(c.h);
+            //$('#SignatureUploadBtn').show();
+
+        };
+        //$(function ($) {
+        //    $('#imgpreview').Jcrop();
+        //});
+
+    </script>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <div>
+              <asp:FileUpload ID="SignatureFileUploader" runat="server"  onchange="showpreview(this);" />
+              <br />
+              <img id="imgpreview"  src="" style="border-width: 0px; visibility: hidden;" />
+              <br />
+              <asp:Button ID="SignatureUploadBtn" runat="server" Text="Upload Signature"  OnClick="SignatureUploadBtn_Click"  />
+              
+        </div>
+        <br />
+        <br />
+       
+    <asp:Label ID="SignatureError" runat="server" Text=""></asp:Label>     
+    <input id="imgX1" type="hidden"  runat ="server" />
+    <input id="imgY1" type="hidden" runat ="server" />
+    <input id="imgWidth" type="hidden" runat ="server"  />
+    <input id="imgHeight" type="hidden"  runat ="server" />
+      
+    </form>
+ 
+     <%--<asp:Image ID="SignatureImage" runat="server" />--%>
+   <%-- <img id="SignatureImage"  src="" style="border-width: 0px"; runat ="server" />--%>
+</body>
+</html>
---
+++ WebApplication3/WebForm1.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using SD = System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+  
+        }
+
+        protected void SignatureUploadBtn_Click(object sender, EventArgs e)
+        {
+            if (SignatureFileUploader.HasFile)
+            {
+                try
+                {
+                    int w =  Convert.ToInt32(imgWidth.Value);
+
+                    int h =  Convert.ToInt32(imgHeight.Value); 
+
+                    int x =  Convert.ToInt32(imgX1.Value);
+
+                    int y = Convert.ToInt32(imgY1.Value);
+
+
+                   
+                    string fileExtension = System.IO.Path.GetExtension(SignatureFileUploader.FileName);
+
+                    if (fileExtension.ToLower() != ".png" && fileExtension.ToLower() != ".jpg" && fileExtension.ToLower() != ".jpeg")
+                    {
+                        SignatureError.Text = "Only files with .doc or .docx extention are allowed";
+                        SignatureError.ForeColor = System.Drawing.Color.Red;
+                    }
+                    else
+                    {
+                        int fileSize = SignatureFileUploader.PostedFile.ContentLength;
+                        if (fileSize >2097152)
+                        {
+                            SignatureError.Text = "Maximum file Size (2MB) exceeded";
+                            SignatureError.ForeColor = System.Drawing.Color.Red;
+                        }
+                        else
+                        {
+                           
+                            var fileName = Guid.NewGuid() + SignatureFileUploader.FileName;
+                            SignatureFileUploader.SaveAs(Server.MapPath("~/Images/" + fileName));
+                            byte[] CropImage = Crop(Server.MapPath("~/Images/" + fileName), w, h, x, y);
+                            using (MemoryStream ms = new MemoryStream(CropImage, 0, CropImage.Length))
+                            {
+                                ms.Write(CropImage, 0, CropImage.Length);
+
+                                using (SD.Image CroppedImage = SD.Image.FromStream(ms, true))
+                                {
+
+                                    CroppedImage.Save(Server.MapPath("~/Images/" + fileName), CroppedImage.RawFormat);
+                                }
+
+                            }
+                                
+                            
+                            //SignatureImage.ImageUrl = Server.MapPath("~/Images/" + fileName);
+                            SignatureError.Text = "File uploaded";
+
+                        }
+                    }
+      
+               
+                }
+                catch (Exception)
+                {
+
+                    SignatureError.Text = "File not uploaded";
+                    SignatureError.ForeColor = System.Drawing.Color.Red;
+                }
+               
+              
+                
+            } 
+            else
+            {
+                SignatureError.Text = "Please select a file to upload";
+                SignatureError.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+
+
+        static byte[] Crop(string Img, int Width, int Height, int X, int Y)
+        {
+
+            try
+            {
+                using (SD.Image OriginalImage = SD.Image.FromFile(Img))
+                {
+                    using (SD.Bitmap bmp = new SD.Bitmap(Width, Height))
+                    {
+                        bmp.SetResolution(OriginalImage.HorizontalResolution, OriginalImage.VerticalResolution);
+
+                        using (SD.Graphics Graphic = SD.Graphics.FromImage(bmp))
+
+                        {
+
+                            Graphic.SmoothingMode = SmoothingMode.AntiAlias;
+
+                            Graphic.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+                            Graphic.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                            Graphic.DrawImage(OriginalImage, new SD.Rectangle(0, 0, Width, Height), X, Y, Width, Height, SD.GraphicsUnit.Pixel);
+
+                            MemoryStream ms = new MemoryStream();
+
+                            bmp.Save(ms, OriginalImage.RawFormat);
+
+                            return ms.GetBuffer();
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+            catch (Exception Ex)
+
+            {
+
+                throw (Ex);
+
+            }
+        }
+
+    }
+}
